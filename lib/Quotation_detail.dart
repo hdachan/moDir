602b71e3b1dbe0d39e3ba03fe62c8fd63a6efa62
황대찬 <ghkdeocks123@naver.com>
2024-08-13 22:27:45 +0900

    20240813 황대찬 스타일링 내역화면 1차

--- lib/Quotation_detail.dart
+++ lib/Quotation_detail.dart
@@ -12,7 +12,8 @@ class QuotationPage extends StatefulWidget {
   _QuotationPageState createState() => _QuotationPageState();
 }
 
-class _QuotationPageState extends State<QuotationPage> with SingleTickerProviderStateMixin {
+class _QuotationPageState extends State<QuotationPage>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   @override
@@ -51,7 +52,10 @@ class _QuotationPageState extends State<QuotationPage> with SingleTickerProvider
             actions: [
               Padding(
                 padding: const EdgeInsets.only(right: 4),
-                child: IconButton(onPressed: () {}, icon: Icon(Icons.home_outlined), iconSize: 24),
+                child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.home_outlined),
+                    iconSize: 24),
               )
             ],
           ),
@@ -60,27 +64,54 @@ class _QuotationPageState extends State<QuotationPage> with SingleTickerProvider
               child: Column(
                 children: [
                   Center(
-                    child: Container(
+                    child:
+                    Container(
                       width: 360,
-                      height: 112,
+                      height: 120,
                       child: Column(
                         children: [
                           Container(
                             width: 360,
-                            height: 46,
-                            color: Colors.tealAccent,
-                            //padding: EdgeInsets.only(left: 16, top: 16),
+                            height: 44,
+                            padding: EdgeInsets.only(left: 16,right:16),
                             child: TabBar(
                               controller: _tabController,
                               isScrollable: true,
                               tabAlignment: TabAlignment.start,
                               tabs: [
-                                Tab(text: '진행중'),
-                                Tab(text: '지난내역'),
+                                Tab(
+                                  child: Text(
+                                    '진행중',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Color(0xFF3D3D3D),
+                                      fontSize: 16,
+                                      fontFamily: 'Pretendard',
+                                      fontWeight: FontWeight.w500,
+                                      height: 1.0,
+                                      letterSpacing: -0.40,
+                                    ),
+                                  ),
+                                ),
+                                Tab(
+                                  child: Text(
+                                    '지난내역',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Color(0xFF3D3D3D),
+                                      fontSize: 16,
+                                      fontFamily: 'Pretendard',
+                                      fontWeight: FontWeight.w500,
+                                      height: 1.0,
+                                      letterSpacing: -0.40,
+                                    ),
+                                  ),
+                                ),
+
                               ],
                               indicatorColor: Color(0xFF3D3D3D),
-                              indicatorSize: TabBarIndicatorSize.tab, // 인디케이터 크기 조정
-                              indicatorPadding: EdgeInsets.only(top: 12, bottom: 0), // 인디케이터와 텍스트 간의 간격 조정
+                              indicatorSize: TabBarIndicatorSize.tab,
+                              indicatorPadding: EdgeInsets.only(top: 12, bottom: 0),
                               labelPadding: EdgeInsets.only(left: 6, right: 6),
                             ),
                           ),
@@ -89,9 +120,63 @@ class _QuotationPageState extends State<QuotationPage> with SingleTickerProvider
                             height: 2.0,
                             thickness: 2.0,
                           ),
+                          Container(
+                            width: 360,
+                            height: 66,
+                            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                            child: Center( // 자식 컨테이너를 중앙 정렬
+                              child: Container(
+                                width: 328,
+                                height: 42,
+                                color: Color(0xFFF6F6F6),
+                                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 18,
+                                      height: 18,
+                                      child: Icon(
+                                        Icons.search,
+                                        size: 18, // 아이콘 크기 설정
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    SizedBox(width: 4),
+                                    Container(
+                                      width: 274,
+                                      height: 18,
+                                      child: TextField(
+                                        decoration: InputDecoration(
+                                          hintText: '제목 및 디자이너, 스타일로 검색해 보세요',
+                                          hintStyle: TextStyle(
+                                            color: Color(0xFF888888), // 힌트 텍스트
+                                            fontSize: 14, // 글자 크기
+                                            fontFamily: 'Pretendard', // 폰트 패밀리
+                                            fontWeight: FontWeight.w400, // 글자 두께
+                                            height: 1.2, // 줄 높이
+                                            letterSpacing: -0.35, // 글자 사이 간격
+                                          ),
+                                          border: InputBorder.none, // 밑줄 없애기
+                                        ),
+                                      ),
+                                    ),
+
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 360,
+                            height: 8,
+                            color: Color(0xFFF6F6F6),
+                          ),
                         ],
                       ),
                     ),
+
+
+
                   ),
                   // 여기 아래에 TabBarView를 추가하여 탭에 따른 내용을 표시할 수 있습니다.
                   Container(
@@ -99,8 +184,8 @@ class _QuotationPageState extends State<QuotationPage> with SingleTickerProvider
                     child: TabBarView(
                       controller: _tabController,
                       children: [
-                        Center(child: Text('메인 탭 내용')),
-                        Center(child: Text('커뮤니티 탭 내용')),
+                        TabContent1(),
+                        TabContent2(),
                       ],
                     ),
                   ),
@@ -111,5 +196,223 @@ class _QuotationPageState extends State<QuotationPage> with SingleTickerProvider
         ),
       ),
     );
+  }
+}
+
+// 첫 번째 탭 내용
+class TabContent1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                // 중간 패널
+                width: 360,
+                height: 276,
+                padding: EdgeInsets.only(top: 16, bottom: 16, left: 16, right: 16),
+                child: Column(
+                  children: [
+                    Container(
+                      width: 328,
+                      height: 22,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 260,
+                            height: 22,
+                            child: Text(
+                              '2024. 08. 09',
+                              style: TextStyle(
+                                color: Color(0xFF3D3D3D), // 텍스트 색상
+                                fontSize: 16, // 글자 크기
+                                fontFamily: 'Pretendard', // 폰트 패밀리
+                                fontWeight: FontWeight.w500, // 글자 두께
+                                height: 1.4, // 줄 높이
+                                letterSpacing: -0.40, // 글자 사이 간격
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 68,
+                            height: 22,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start, // 왼쪽 정렬
+                              children: [
+                                Text(
+                                  '상세내역',
+                                  style: TextStyle(
+                                    color: Color(0xFF888888), // 텍스트 색상
+                                    fontSize: 14, // 글자 크기
+                                    fontFamily: 'Pretendard', // 폰트 패밀리
+                                    fontWeight: FontWeight.w400, // 글자 두께
+                                    height: 1.2, // 줄 높이
+                                    letterSpacing: -0.35, // 글자 사이 간격
+                                  ),
+                                ),
+                                SizedBox(width: 2), // 텍스트와 아이콘 사이에 2의 간격 추가
+                                Icon(
+                                  Icons.arrow_forward, // 아이콘 설정
+                                  size: 18, // 아이콘 크기
+                                  color: Color(0xFF888888), // 아이콘 색상 (원하는 경우 조정 가능)
+                                ),
+                              ],
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(height: 8), // 두 컨테이너 사이의 간격을 조정
+                    Container(
+                      width: 328,
+                      height: 210,
+                      padding: EdgeInsets.only(top: 12, bottom: 12, left: 16, right: 16),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 296,
+                            height: 22,
+                            color: Colors.blue, // 첫 번째 박스 색상
+                          ),
+                          SizedBox(height:12),
+                          Container(
+                            width: 296,
+                            height: 98,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 98,
+                                  height: 98,
+                                  child: Image.asset(
+                                    'assets/image/1111.png',
+                                    fit: BoxFit.cover, // 이미지 크기 조정 방식 (필요에 따라 조정 가능)
+                                  ),
+                                ),
+                                SizedBox(width: 16), // 컨테이너 사이의 간격
+                                Container(
+                                  width: 182,
+                                  height: 98,
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        width: 182,
+                                        height: 20,
+                                        child: Text(
+                                          '코디 맛있게 해드려요!',
+                                          style: TextStyle(
+                                            color: Color(0xFF3D3D3D), // 텍스트 색상
+                                            fontSize: 14, // 글자 크기
+                                            fontFamily: 'Pretendard', // 폰트 패밀리
+                                            fontWeight: FontWeight.w600, // 글자 두께
+                                            height: 1.4, // 줄 높이
+                                            letterSpacing: -0.35, // 글자 사이 간격
+                                          ),
+                                        ),
+                                      ),
+
+                                      SizedBox(height: 8),
+                                      Container(
+                                        width: 182,
+                                        height: 17,
+                                        child: Text(
+                                          '인혜엄',
+                                          style: TextStyle(
+                                            color: Color(0xFF3D3D3D), // 텍스트 색상
+                                            fontSize: 14, // 글자 크기
+                                            fontFamily: 'Pretendard', // 폰트 패밀리
+                                            fontWeight: FontWeight.w400, // 글자 두께
+                                            height: 1.2, // 줄 높이
+                                            letterSpacing: -0.35, // 글자 사이 간격
+                                          ),
+                                        ),
+                                      ),
+
+                                      SizedBox(height: 8),
+                                      Container(
+                                        width: 182,
+                                        height: 17,
+                                        color: Colors.yellow, // 세 번째 내부 컨테이너 색상 (원하는 색상으로 조정 가능)
+                                      ),
+                                      SizedBox(height: 8),
+                                      Container(
+                                        width: 182,
+                                        height: 20,
+                                        child: Text(
+                                          '20,000원',
+                                          style: TextStyle(
+                                            color: Color(0xFF3D3D3D), // 텍스트 색상
+                                            fontSize: 14, // 글자 크기
+                                            fontFamily: 'Pretendard', // 폰트 패밀리
+                                            fontWeight: FontWeight.w600, // 글자 두께
+                                            height: 1.4, // 줄 높이
+                                            letterSpacing: -0.35, // 글자 사이 간격
+                                          ),
+                                        ),
+                                      ),
+
+                                    ],
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                          ),
+
+                          SizedBox(height:12),
+                          Container(
+                            width: 296,
+                            height: 42,
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4), // 둥근 모서리 설정
+                              border: Border.all(
+                                color: Color(0xFFE7E7E7), // 테두리 색상 설정
+                                width: 1, // 테두리 두께
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '분석취소',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Color(0xFF3D3D3D), // 텍스트 색상
+                                  fontSize: 14, // 글자 크기
+                                  fontFamily: 'Pretendard', // 폰트 패밀리
+                                  fontWeight: FontWeight.w500, // 글자 두께
+                                  height: 1.4, // 줄 높이
+                                  letterSpacing: -0.35, // 글자 사이 간격
+                                ),
+                              ),
+                            ),
+                          ),
+
+
+
+                        ],
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+// 두 번째 탭 내용
+class TabContent2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('커뮤니티 탭 내용'));
   }
 }
